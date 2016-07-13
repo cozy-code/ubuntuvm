32272bcf7d5590569de763ff1062d56dca4c61db
cozy code <cozy.code@outlook.com>
2016-07-13 14:37:09 +0900

    add docker install

--- provision/docker.sh
+++ provision/docker.sh
@@ -3,16 +3,27 @@
 set -x
 
 #load config file
-source ~/provision/config_value
+. ~/provision/config_value
 # install docker https://docs.docker.com/engine/installation/linux/ubuntulinux/
 sudo apt-get -y update
 sudo apt-get -y install apt-transport-https ca-certificates
 
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
-echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" > /etc/apt/sources.list.d/docker.list
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
 
 sudo apt-get -y update
 sudo apt-get -y purge lxc-docker
 
 sudo apt-get -y install linux-image-extra-$(uname -r)
+sudo apt-get -y install docker-engine
+
+#start
+sudo service docker start
+#Docker to start on boot
+sudo systemctl enable docker
+
+#enable non-root user access docker
+sudo groupadd docker
+sudo usermod -aG docker $WORKING_USER
+
