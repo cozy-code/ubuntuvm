6d952af83c3a23093b02d89bd40492470756ebf1
cozy code <cozy.code@outlook.com>
2016-07-13 17:29:44 +0900

    add docker network config

--- provision/docker.sh
+++ provision/docker.sh
@@ -27,3 +27,18 @@ sudo systemctl enable docker
 sudo groupadd docker
 sudo usermod -aG docker $WORKING_USER
 
+#Enable UFW forwarding
+TIMESTAMP=`date -u +%s`
+sudo cp /etc/default/ufw /etc/default/ufw.$TIMESTAMP.back
+
+EDIT="s/^DEFAULT_FORWARD_POLICY=.*$/DEFAULT_FORWARD_POLICY=\"ACCEPT\"/"
+cat /etc/default/ufw  | sed "$EDIT"  | sudo tee /etc/default/ufw
+sudo ufw reload
+sudo ufw allow 2375/tcp
+
+#Configure a DNS server for use by Docker
+
+if !( grep "^ *DOCKER_OPTS=" /etc/default/docker > /dev/null ); then
+    echo "DOCKER_OPTS=\"--dns 8.8.8.8 --dns 8.8.4.4\"" | sudo tee -a /etc/default/docker
+    sudo service docker restart
+fi
